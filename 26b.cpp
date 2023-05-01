a902ef542f1543b4296f29a94fb1e74fdb8147ec
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-05-01 20:59:58 +0300

    Create 26b.cpp

---
+++ 26b.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    string input;
+    set<char> soglasnie{ 'б', 'в', 'г', 'д', 'ж', 'з', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ' };
+    set<char> used_soglasnie;
+
+
+    getline(cin, input);
+
+
+    for (size_t i = 0; i < input.length(); i++) {
+
+        size_t start = i;
+        // Поиск конца слова
+        while (i < input.length() && input[i] != ',' && input[i] != '.') {
+            i++;
+        }
+
+        string word = input.substr(start, i - start);
+
+        // Поиск согласных букв в текущем слове
+        set<char> word_soglasnie;
+        for (char c : word) {
+            if (soglasnie.count(c)) {
+                word_soglasnie.insert(c);
+            }
+        }
+        used_soglasnie.insert(word_soglasnie.begin(), word_soglasnie.end());
+    }
+
+    // Поиск согласных букв, которые не входят ни в одно слово
+    set<char> unused_soglasnie;
+    for (char c : soglasnie) {
+        if (!used_soglasnie.count(c)) {
+            unused_soglasnie.insert(c);
+        }
+    }
+
+    // Печатаем согласные буквы, которые не входят ни в одно слово
+    for (char c : unused_soglasnie) {
+        cout << c << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
